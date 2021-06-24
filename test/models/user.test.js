3fb4acf63922012dac5312912c982d3fffa2cb8a
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2021-06-24 13:58:59 +0300

    Use property based testing in user.test.js.

--- test/models/user.test.js
+++ test/models/user.test.js
@@ -1,30 +1,28 @@
 const fs = require("fs");
+const fc = require("fast-check");
 
 const User = require("../../models/user.js").User;
 
 fs.rmdirSync("./private/db", {recursive: true});
+jest.setTimeout(10000);
 
-User.secret = "TEST_SECRET";
-const user = new User("ba@bu.bi");
-const password = "1948";
+test("can add a user and then login", () => fc.assert(fc.asyncProperty(
+    fc.string({minLength: 1}),
+    fc.emailAddress().filter(email => !email.includes("/")),
+    fc.string(),
+    fc.string(),
+    async (secret, email, password, fakePassword) => {
+        fc.pre(password !== fakePassword);
 
-test("can add a user", async () => {
-    await user.setPassword(password);
-});
+        User.secret = secret;
+        const user = new User(email);
+        await user.setPassword(password);
 
-test("can login", async () => {
-    const token = await user.login(password);
-    expect(token).toBeTruthy();
-});
+        let token = await user.login(password);
+        expect(token).toBeTruthy();
+        expect(typeof token).toEqual("string");
 
-test("cannot login with a fake password", async () => {
-    const fakePasswords = [
-        password + password,
-        password + " ",
-        password.slice(1),
-    ];
-    for (fakePassword of fakePasswords) {
-        const token = await user.login(fakePassword);
+        token = await user.login(fakePassword);
         expect(token).toBe(null);
-    }
-});
+    }))
+);
