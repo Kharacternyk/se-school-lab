a7cf46c83db648a7f17b7bcf2ff18a368cbe2fea
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2021-06-24 14:35:05 +0300

    Cover the token stuff in the tests.

--- test/models/user.test.js
+++ test/models/user.test.js
@@ -11,18 +11,22 @@ test("can add a user and then login", () => fc.assert(fc.asyncProperty(
     fc.emailAddress().filter(email => !email.includes("/")),
     fc.string(),
     fc.string(),
-    async (secret, email, password, fakePassword) => {
-        fc.pre(password !== fakePassword);
-
+    fc.string(),
+    async (secret, email, password, fakePassword, fakeToken) => {
         User.secret = secret;
         const user = new User(email);
         await user.setPassword(password);
 
-        let token = await user.login(password);
+        const token = await user.login(password);
         expect(token).toBeTruthy();
         expect(typeof token).toEqual("string");
+        expect(User.authenticate(token)).toEqual(user);
 
-        token = await user.login(fakePassword);
-        expect(token).toBe(null);
+        if (password !== fakePassword) {
+            expect(await user.login(fakePassword)).toBe(null);
+        }
+        if (token !== fakeToken) {
+            expect(User.authenticate(fakeToken)).toBe(null);
+        }
     }))
 );
